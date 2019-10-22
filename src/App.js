37e6a2b6c68d237dc272d8bd6286d24f17431de3
k2project <kopczkrzy@gmail.com>
2019-10-22 22:47:53 +0100

    Skills responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
 import Header from './Header/Header';
 import About from './About/About';
@@ -8,6 +8,9 @@ import Resume from './Resume/Resume';
 import Footer from './Footer/Footer';
 
 import projects from './projects';
+
+//for resize function
+let projectsSelected = [];
 
 function App() {
     const [projectsList, setProjectsList] = useState(projects)
@@ -21,23 +24,45 @@ function App() {
                 }
             })
             setProjectsList(newProjectsList)
-            console.log(newProjectsList)
+            projectsSelected = newProjectsList;
+            return newProjectsList;
         //
         }else{
             setProjectsList(projects)
+            projectsSelected = [];
         }
 
         // document.querySelector('.Projects').scrollIntoView();
 
     }
+    function handleResize(){
+        if(document.documentElement.clientWidth > 1024){
+            if(projectsSelected.length>0){
+                setProjectsList(projectsSelected);
+            }else{
+
+                setProjectsList(projects)
+            }
+
+        }else{
+            setProjectsList(projects)
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener('resize', handleResize);
+        return ()=>{
+            window.removeEventListener('resize', handleResize);
+
+        }
+    })
   return (
     <div className="Portfolio">
         <Header/>
         <About/>
         <Skills setSkills={setSkills}/>
-        {/* <Projects projects={projectsList}/>
+        <Projects projects={projectsList}/>
         <Resume/>
-        <Footer/> */}
+        <Footer/>
     </div>
   );
 }
