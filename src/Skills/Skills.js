bb5a778f2dfb10d4f0b2259c4f763795dae8b124
k2project <kopczkrzy@gmail.com>
2019-10-22 18:12:18 +0100

    skills mob icons

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -2,6 +2,17 @@ import React, {useEffect} from 'react';
 import './Skills.scss';
 import Button from './../components/Button/Button';
 
+import reactIcon from './../media/icons/reactIcon.png';
+import css3Icon from './../media/icons/css3Icon.png';
+import html5Icon from './../media/icons/html5Icon.png';
+import jsIcon from './../media/icons/jsIcon.png';
+import sass from './../media/icons/sass.png';
+import gitHub from './../media/icons/gitHub.png';
+import heroku from './../media/icons/heroku.png';
+import jQuery from './../media/icons/jQuery.png';
+import meteor from './../media/icons/meteor.png';
+import nodeJs from './../media/icons/nodeJs.png';
+
 const mainSkillsList = ['CSS3', 'HTML5', 'JavaScript', 'React'];
 const otherSkillsList = [ 'jQuery','Node.js', 'Meteor.js', 'GitHub', 'Heroku','Sass', 'SVG', 'Canvas', 'CSS Grid/Flexbox', 'Responsive Design',];
 
@@ -52,11 +63,30 @@ function Skills(props) {
   return (
     <div className="Skills" id="skills">
         <div className="wrapper">
-            <div className="Skills_main">
-                {mainSkills}
+            <div className="Skills__desk">
+                <div className="Skills__desk_main">
+                    {mainSkills}
+                </div>
+                <div className="Skills__desk_other">
+                    {otherSkills}
+                </div>
             </div>
-            <div className="Skills_other">
-                {otherSkills}
+            <div className="Skills__mob">
+                <div className="Skills__mob_main">
+                    <img src={css3Icon} alt="skilled in css3"/>
+                    <img src={html5Icon} alt="skilled in html5"/>
+                    <img src={jsIcon} alt="skilled in javascript"/>
+                    <img src={reactIcon} alt="skilled in react"/>
+                </div>
+                <div className="Skills__mob_other">
+                    <img src={sass} alt="skilled in sass"/>
+                    <img src={gitHub} alt="skilled in github"/>
+                    <img src={heroku} alt="skilled in heroku"/>
+                    <img src={jQuery} alt="skilled in jquery"/>
+                    <img src={nodeJs} alt="skilled in nodejs"/>
+                    <img src={meteor} alt="skilled in meteor"/>
+
+                </div>
             </div>
         </div>
     </div>
