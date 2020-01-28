99f719e0a681e9721f265e34ad8628aff47ffa80
k2project <kopczkrzy@gmail.com>
2020-01-28 14:28:35 +0000

    post links

--- src/Blog/posts/accessibility_links.js
+++ src/Blog/posts/accessibility_links.js
@@ -23,12 +23,18 @@ const accessibility_links={
             <section className="post__section">
                 <h3 className="post__heading">Links Accessibility Best Practices</h3>
                 <ul className="post__list">
-                    <li>Avoid using JavaScript event handlers that do not permit keyboard access. <i>In most browsers, the <b>Tab</b> key allows users to jump from link to link, and the <b>Enter</b> key allows users to select a link. </i></li>
+                    <li><b>Use good link's text.</b> Links should make sense out of context. Avoid phrases such as "click here", "more", "click for details".</li>
+                    <li><b>Links should never be empty! </b> Their text should be as concise as possible without sacrificing the meaning.</li>
+                    <li>Place the distinguishing information of links at the beginning of a link. Don't put extra information first, like "Link opens in a new window: Products." Instead, say "Products (opens in a new window)".</li>
+                     <li>Links do not need to include "link" in the link text. Screen readers generally inform users that a piece of text (or a graphic) is a link. And with graphics used as links, the alt text does not need to say "link" neither. </li>
+                     <li>Use human-readable text instead of the URL as they are more screen-reader friendly.</li>
+                    <li>Avoid using JavaScript event handlers that do not permit keyboard access. <br/> <i>In most browsers, the "TAB" key allows users to jump from link to link, and the "ENTER" key allows users to select a link. </i></li>
                     <li>Steer clear of creating links that go nowhere, such as dropdown menu on mouse action.
                         <span className="post__card">
                             <code>&lt;a href="#" onmouseover="dropdownmenu()" &gt; Submenu &lt;/a&gt;</code>
                         </span>
                      </li>
+
                 </ul>
             </section>
             <section className="post__section"></section>
