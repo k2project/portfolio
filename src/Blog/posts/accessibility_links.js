1868c6c0d71d9971d7ba37fd891c8cdf081905f0
k2project <kopczkrzy@gmail.com>
2020-01-28 16:10:47 +0000

    post links

--- src/Blog/posts/accessibility_links.js
+++ src/Blog/posts/accessibility_links.js
@@ -22,20 +22,87 @@ const accessibility_links={
             </section>
             <section className="post__section">
                 <h3 className="post__heading">Links Attributes</h3>
-                <div className="post__card">
-                    <table className="post__table">
-                        <tr>
-                            <th>Attribute</th>
-                            <th>Value</th>
-                            <th>Description</th>
-                        </tr>
-                        <tr>
-                            <td>href</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </table>
-                </div>
+                <table className="post__table">
+                    <tr>
+                        <th>Attribute</th>
+                        <th>Value</th>
+                        <th>Description</th>
+                    </tr>
+                    <tr>
+                        <td>href</td>
+                        <td>url</td>
+                        <td>Linked document, resource, or location.</td>
+                    </tr>
+                    <tr>
+                        <td>hreflang</td>
+                        <td></td>
+                        <td>Specifies the language of the linked resource.</td>
+                    </tr>
+                    <tr>
+                        <td>download</td>
+                        <td></td>
+                        <td>Directs the browser to download the linked resource rather than opening it.</td>
+                    </tr>
+                    <tr>
+                        <td>target</td>
+                        <td>
+                            _blank <br/>
+                            _parent <br/>
+                            _self <br/>
+                            _top <br/>
+                            frame name
+                        </td>
+                        <td>Specifies the context in which the linked resource will open.</td>
+                    </tr>
+                    <tr>
+                        <td>title</td>
+                        <td>text</td>
+                        <td>Defines the title of a link, which appears to the user as a tooltip.</td>
+                    </tr>
+                    <tr>
+                        <td>media</td>
+                        <td>
+                            print <br/>
+                            screen <br/>
+                            aural <br/>
+                            braille <br/>
+                        </td>
+                        <td>Allows the user agent to pick the best adapted one for the device it runs on</td>
+                    </tr>
+                    <tr>
+                        <td>size</td>
+                        <td>
+                            any <br/>
+                            16x16 <br/>
+                            16x16 32x32 64x64 <br/>
+                        </td>
+                        <td>Specifies the sizes of icons for visual media. Can be used for favicons. Multiple values can be provided, as long as they're separated by a space. </td>
+                    </tr>
+                    <tr>
+                        <td>type</td>
+                        <td>image/png</td>
+                        <td>The MIME type of content at the link destination </td>
+                    </tr>
+                    <tr>
+                        <td>rel</td>
+                        <td>
+                            stylesheet <br/>
+                            noopener* <br/>
+                            noreferrer** <br/>
+                            icon <br/>
+                        </td>
+                        <td>Denotes how the item being linked to is related to the containing document.</td>
+                    </tr>
+                    <tr>
+                        <td>tabindex</td>
+                        <td>number (-1, 0, int)</td>
+                        <td>Defines an explicit tab order.Link with tabindex value of "-1" can't be reached by the keyboard.</td>
+                    </tr>
+                </table>
+                <p>
+                    <small>*Instructs the browser to open the link without granting the new browsing context access to the document that opened it — by not setting the Window.opener property on the opened window (it returns null).This is especially useful when opening untrusted links.</small><br/>
+                    <small>**Prevents the browser, when navigating to another page, to send this page address, or any other value, as referrer via the Referer: HTTP header.</small>
+                </p>
             </section>
             <section className="post__section">
                 <h3 className="post__heading">Links Accessibility Best Practices</h3>
