ee2b47e7aafbfd2e1f0a828fcc59294f254f9857
k2project <kopczkrzy@gmail.com>
2020-02-06 12:42:03 +0000

    checklist page

--- src/Checklist/Checklist.js
+++ src/Checklist/Checklist.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './Checklist.scss';
+import AccordionWidget from './../components/AccordionWidget/AccordionWidget';
+import Accordion from '../components/Accordion/Accordion';
 
 function Checklist() {
     useEffect(()=>{
       const selectedItemsArr = JSON.parse(localStorage.getItem("checklist"));
       const listItems = document.getElementsByClassName('checklist__item');
-      if(selectedItemsArr){
+      if(selectedItemsArr && listItems){
         Array.from(listItems).forEach((item)=>{
             const itemId = item.getAttribute('id');
             if(selectedItemsArr.includes(itemId)){
@@ -16,6 +18,7 @@ function Checklist() {
     })
     return(
         <article className="checklist">
+          <h2 className="checklist__title">Web Accesibilty Checklist</h2>
           <SemanticStructure/>
         </article>
     )
@@ -54,26 +57,34 @@ function ChecklistLiEl(props){
 }
 
 function SemanticStructure(){
+  const accordionData={
+    heading:3,
+    title:'Page Semantic Structure',
+    subtitle:null,
+    styling:'bg-grey',
+  }
   return(
-    <section className="checklist__section">
-        <h2 className="checklist__caption">Page Semantic Structure</h2>
+    <AccordionWidget data={accordionData}>
         <ul className="checklist__list">
-          <h3 className="checklist__list-caption">Page Regions</h3>
-          <ChecklistLiEl id="item-1"> first item</ChecklistLiEl>
-          <ChecklistLiEl id="item-2"> second item</ChecklistLiEl>
+          <h4 className="checklist__list-caption">Page Regions</h4>
+          <ChecklistLiEl id="regions-1"> Mark up different regions of applications, so that they can be identified by web browsers and assistive technologies.</ChecklistLiEl>
+          <h5 className="checklist__list-subcaption">___Page Header :</h5>
+          <ChecklistLiEl id="regions-2"><em>&lt;header&gt;</em> element present at the top of every page for the website logo, search function, and navigation options. </ChecklistLiEl>
+          <ChecklistLiEl id="regions-3"><em>&lt;header&gt;</em> used inside <em>&lt;article&gt;</em> and <em>&lt;section&gt;</em> elements, not to be associated with the whole page, but only with that specific element.</ChecklistLiEl>
+          <h5 className="checklist__list-subcaption">___Page Footer :</h5>
+          <ChecklistLiEl id="regions-4"><em>&lt;footer&gt;</em> element present at the bottom of every page that contains site-wide information, such as copyright information, privacy statements, or disclaimers.</ChecklistLiEl>
+          <ChecklistLiEl id="regions-5"><em>&lt;footer&gt;</em> used inside <em>&lt;article&gt;</em> and <em>&lt;section&gt;</em> elements, not to be associated with the whole page, but only with that specific element.</ChecklistLiEl>
+          <ChecklistLiEl id="regions-"> </ChecklistLiEl>
         </ul>
         <ul className="checklist__list">
-          <h3 className="checklist__list-caption">Labeling Regions</h3>
-          <li></li>
+          <h4 className="checklist__list-caption">Labeling Regions</h4>
         </ul>
         <ul className="checklist__list">
-          <h3 className="checklist__list-caption">Headings</h3>
-          <li></li>
+          <h4 className="checklist__list-caption">Headings</h4>
         </ul>
         <ul className="checklist__list">
-          <h3 className="checklist__list-caption">Content Structure</h3>
-          <li></li>
+          <h4 className="checklist__list-caption">Content Structure</h4>
         </ul>
-    </section>
+    </AccordionWidget>
   )
 }
