f515c8e8ba79a20331ff2c912d8cbd43f5be36d4
k2project <kopczkrzy@gmail.com>
2020-02-10 12:12:02 +0000

    checklist page imgs

--- src/Checklist/accordions/Images.js
+++ src/Checklist/accordions/Images.js
@@ -53,14 +53,39 @@ function Images() {
                     <h4 className="checklist__subcaption" id="#functional-img">Functional Images</h4>
                     <ul className="checklist__list" aria-labelledby="functional-img">
                          <ChecklistLiEl id="f-img-1">
+                              Functional images are used to initiate actions rather than to convey information. They are used in buttons, links, and other interactive elements.
                          </ChecklistLiEl>
                          <ChecklistLiEl id="f-img-2">
+                              The text alternative for the image should convey the action that will be initiated.
                          </ChecklistLiEl>
                          <ChecklistLiEl id="f-img-3">
+                              Using image alone as logo in a link contains alt attribute describing the website name and page it likns to (egz,. W3C home).
                          </ChecklistLiEl>
                          <ChecklistLiEl id="f-img-4">
+                              For logo image within link text supply alt="" to avoid redundancy and repetition.
                          </ChecklistLiEl>
                          <ChecklistLiEl id="f-img-5">
+                              Icon images conveying information within links text or buttons and stand-alone icon images that have a function (egz., file format, opens in a new tab) should have descriptive alt attribute.
+                         </ChecklistLiEl>
+                    </ul>
+               </section>
+               <section>
+                    <h4 className="checklist__subcaption" id="#text-img">Images Of Text</h4>
+                    <ul className="checklist__list" aria-labelledby="text-img">
+                         <ChecklistLiEl id="txt-img-1">
+
+                         </ChecklistLiEl>
+                         <ChecklistLiEl id="txt-img-2">
+
+                         </ChecklistLiEl>
+                         <ChecklistLiEl id="txt-img-3">
+
+                         </ChecklistLiEl>
+                         <ChecklistLiEl id="txt-img-4">
+
+                         </ChecklistLiEl>
+                         <ChecklistLiEl id="txt-img-5">
+
                          </ChecklistLiEl>
                     </ul>
                </section>
