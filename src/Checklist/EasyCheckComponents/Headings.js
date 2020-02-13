f2e36ce3ab473f3a44692cb1a2d29f022b01d86d
k2project <kopczkrzy@gmail.com>
2020-02-13 14:31:15 +0000

    checklist page easy checklist

--- src/Checklist/EasyCheckComponents/Headings.js
+++ src/Checklist/EasyCheckComponents/Headings.js
@@ -17,7 +17,24 @@ function Headings() {
                     className='checklist__list'
                     aria-label='easy checklist page headings'
                 >
-                    <ChecklistLiEl id='aec-hadings-1'></ChecklistLiEl>
+                    <ChecklistLiEl id='aec-hadings-1'>
+                        The page has a heading. In almost all pages there should
+                        be at least one heading.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-hadings-2'>
+                        There is only one <i>h1</i> heading per page.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-hadings-3'>
+                        All text that looks like a heading is marked up as a
+                        heading.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-hadings-4'>
+                        All text that is marked up as a heading is really a
+                        conceptual section heading.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-hadings-5'>
+                        The heading hierarchy is meaningful.
+                    </ChecklistLiEl>
                 </ul>
             </section>
         </AccordionWidget>
--- src/Checklist/EasyCheckComponents/ImgsWithAlt.js
+++ src/Checklist/EasyCheckComponents/ImgsWithAlt.js
@@ -17,7 +17,50 @@ function ImgsWithAlt() {
                     className='checklist__list'
                     aria-label='easy checklist page imgages with text alternative'
                 >
-                    <ChecklistLiEl id='aec-img-1'></ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-1'>
+                        Every image has alt with appropriate alternative text.
+                    </ChecklistLiEl>
+
+                    <ChecklistLiEl id='aec-img-2'>
+                        If an image conveys information useful for interacting
+                        with or understanding the web page content, then it
+                        needs alternative text.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-3'>
+                        If an image is just decorative and people don't need to
+                        know about the image, then it should have null alt
+                        (alt="").
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-4'>
+                        The text should be functional and provide an equivalent
+                        user experience, not necessarily describe the image.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-5'>
+                        The text needs to convey the same meaning as the image.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-6'>
+                        Alternative text depends on context.{' '}
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-7'>
+                        Images that are functional — for example, images that
+                        initiate actions (like submit buttons) and linked images
+                        (like in navigation) — need alt text that is the
+                        functional equivalent.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-8'>
+                        If there is text in the image (logo) that text needs to
+                        be included in the alt text.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-9'>
+                        If the image has complex information — such as charts or
+                        graphs — the image should have a short alt text to
+                        identify the image, and then the detailed description of
+                        the information should be provided elsewhere.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-img-10'>
+                        he alternative text does not need to include the words
+                        "button", "link", or "image of".
+                    </ChecklistLiEl>
                 </ul>
             </section>
         </AccordionWidget>
