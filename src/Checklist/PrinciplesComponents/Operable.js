de6aae4c54d2aa419fbe652d964ae6366d2a7c15
k2project <kopczkrzy@gmail.com>
2020-02-13 12:53:04 +0000

    checklist page operable

--- src/Checklist/PrinciplesComponents/Operable.js
+++ src/Checklist/PrinciplesComponents/Operable.js
@@ -83,7 +83,19 @@ function Operable() {
                     className='checklist__list'
                     aria-labelledby='users-seizures'
                 >
-                    <ChecklistLiEl id='users-seizures-1'></ChecklistLiEl>
+                    <ChecklistLiEl id='users-seizures-1'>
+                        Do not include content that flashes at particular rates
+                        and patterns (more than 3 times per second, and flashes
+                        fall below the general flash thresholds).
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-seizures-2'>
+                        Warn users before flashing content is presented, and
+                        provide alternatives.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-seizures-3'>
+                        Provide mechanisms to switch off animations, unless they
+                        are essential.
+                    </ChecklistLiEl>
                 </ul>
             </section>
             <section>
@@ -92,7 +104,34 @@ function Operable() {
                     they are
                 </h4>
                 <ul className='checklist__list' aria-labelledby='users-nav'>
-                    <ChecklistLiEl id='users-nav-1'></ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-1'>
+                        Provide well organized content to help users to orient
+                        themselves and to navigate effectively.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-2'>
+                        Pages have clear titles and are organized using
+                        descriptive section headings.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-3'>
+                        There is more than one way to find relevant pages within
+                        a set of web pages (sitemap,headings).
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-4'>
+                        Users are informed about their current location within a
+                        set of related pages (breadcrumps).
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-5'>
+                        There are ways to bypass blocks of content that are
+                        repeated on multiple pages (<q>skip to main</q> link).
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-6'>
+                        The keyboard focus is visible, and the focus order
+                        follows a meaningful sequence.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-nav-7'>
+                        The purpose of a link is evident, ideally even when the
+                        link is viewed on its own.
+                    </ChecklistLiEl>
                 </ul>
             </section>
             <section>
@@ -103,7 +142,26 @@ function Operable() {
                     className='checklist__list'
                     aria-labelledby='users-modalities'
                 >
-                    <ChecklistLiEl id='users-modalities-1'></ChecklistLiEl>
+                    <ChecklistLiEl id='users-modalities-1'>
+                        Gestures that require dexterity or fine movement have
+                        alternatives that do not require high dexterity.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-modalities-2'>
+                        Components are designed to avoid accidental activation,
+                        for example by providing undo functionality.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-modalities-3'>
+                        Labels presented to users match corresponding object
+                        names in the code, to support activation by voice.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-modalities-4'>
+                        Functionality that is activated by movement can also be
+                        activated through user interface components.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='users-modalities-5'>
+                        Buttons, links, and other active components are large
+                        enough to make them easier to activate by touch.
+                    </ChecklistLiEl>
                 </ul>
             </section>
         </AccordionWidget>
