47ed3438fcbf22ac0f843b9d40d2554505f7c778
k2project <kopczkrzy@gmail.com>
2020-02-14 11:57:21 +0000

    structure basic check added to checklist page

--- src/Checklist/EasyCheckComponents/Structure.js
+++ src/Checklist/EasyCheckComponents/Structure.js
@@ -17,8 +17,37 @@ function Structure() {
                     className='checklist__list'
                     aria-label='easy checklist structure'
                 >
-                    <ChecklistLiEl id='aec-structure-1'></ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-1'>
+                        Check the web page without images, styles, and layout.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-2'>
+                        Turn off images and show the text alternatives.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-3'>
+                        Turn off style sheets (CSS), which specifies how the
+                        page is displayed with layout, colors, etc.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-4'>
+                        Linearize the page or the tables (depending on the
+                        toolbar).
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-5'>
+                        Check that the information makes sense when read in the
+                        order it is shown.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-6'>
+                        Check that the alternative text provides adequate
+                        information for the missing images.
+                    </ChecklistLiEl>
+                    <ChecklistLiEl id='aec-structure-7'>
+                        Check that blocks of information have clear headings.
+                    </ChecklistLiEl>
                 </ul>
+                <small>
+                    NOTE: Data tables will not make sense when linearized.
+                    However, the screen readers have functionality to make data
+                    tables usable (when they are marked up correctly).
+                </small>
             </section>
         </AccordionWidget>
     );
