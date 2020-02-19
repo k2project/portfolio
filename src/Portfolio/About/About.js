bdc913914d61a0fd055c691a70e27798b270131f
k2project <kopczkrzy@gmail.com>
2020-02-19 12:32:00 +0000

    about section accessable

--- src/Portfolio/About/About.js
+++ src/Portfolio/About/About.js
@@ -2,21 +2,39 @@ import React from 'react';
 import './About.scss';
 
 function About() {
-  return (
-    <div className="About" id="about">
-        <div className="wrapper">
-            <p className="p_larger">
-                <b>
-                    Hello!  <br/>
-                    I'm Kris, an Edinburgh based front-end web developer passionate about <span className="color_themed_secondary">React</span> and <span className="color_themed_secondary">Web Accessibility</span>.
-                </b>
-            </p>
-            <p className="p_larger">
-                     I enjoy turning complex problems into simple, beautiful and intuitive applications. My currrent goal is to learn how to make my work a better experience for every user!
-            </p>
-        </div>
-    </div>
-  );
+    return (
+        <section className='about' id='about'>
+            <div className='about__heading'>
+                <div className='wrapper'>
+                    <h1>
+                        Edinburgh based front-end web developer passionate about
+                        coding with{' '}
+                        <span className='color_themed_secondary'>React</span> .{' '}
+                        <br />
+                        <span className='color_themed_secondary'>
+                            Web Accessibility
+                        </span>
+                        , Usability, and Inclusion advocate.
+                    </h1>
+                </div>
+            </div>
+            <div className='about__desc'>
+                <div className='wrapper'>
+                    <p> Hello, I'm Kris! </p>
+                    <p>
+                        I take proud in being self-taught developer. I enjoy
+                        turning complex problems into simple, beautiful and
+                        intuitive applications. Currrently learning how to make
+                        my work a better experience to every user!
+                    </p>
+                    <p>
+                        <b>On the lookout for a team.</b> A team that I can
+                        learn from, grow with and contribute to it success.{' '}
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default About;
