8e6d3ce2b36f224c5b137a2577dc7c19e903e1be
k2project <kopczkrzy@gmail.com>
2020-02-27 11:36:42 +0000

    skills list added to project link

--- src/Portfolio/Projects/Projects.js
+++ src/Portfolio/Projects/Projects.js
@@ -123,9 +123,7 @@ function Project({ project: { name, desc, img, web, git, cml, skills } }) {
                 </div>
                 <div className='project__info'>
                     {desc}
-                    <span className='project__arr' aria-hidden='true'>
-                        &#8594;
-                    </span>
+                
                     <ul className="project__skills-list" aria-label={`skills utilised within the ${name} project`}>{skills.map((skill,i)=><li key={`skillList-${name}-${i}`}><span aria-hidden="true">&#10004;</span> {skill}</li>)}</ul>
                 </div>
             </div>
